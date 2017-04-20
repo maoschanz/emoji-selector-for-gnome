d9a4d1ad2ae0c768c0acbb4326ca203f64eb41fd
Maestroschan <rrroschan@gmail.com>
2017-04-20 22:59:44 +0200

    cosmetic changes

--- prefs.js
+++ prefs.js
@@ -12,78 +12,84 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
+//-----------------------------------------------
+
 function init() {
     Convenience.initTranslations();
 }
 
+//-----------------------------------------------
+
 const EmojiSettingsWidget = new GObject.Class({
     Name: 'EmojiSelector.Prefs.Widget',
     GTypeName: 'EmojiSelectorPrefsWidget',
-    Extends: Gtk.Grid,
+    Extends: Gtk.Box,
 
     _init: function(params) {
 		this.parent(params);
         this.margin = 30;
-        this.row_spacing = this.column_spacing = 12;
+        this.spacing = 25;
+        this.fill = true;
         this.set_orientation(Gtk.Orientation.VERTICAL);
 
 		this._settings = Convenience.getSettings();
 
-		let label_main = '<b>' + _("Modifications will be effective after restarting the extension.") + '</b>';
-		this.add(new Gtk.Label({ label: label_main, use_markup: true, halign: Gtk.Align.START }));
+		let labelMain = '<b>' + _("Modifications will be effective after reloading the extension.") + '</b>';
+		this.add(new Gtk.Label({ label: labelMain, use_markup: true, halign: Gtk.Align.START }));
 		
-		//-------------------------------
+		//------------------------------------------
 		
-		let label_pos_recent = _("Display of recent emojis :");
-		this.add(new Gtk.Label({ label: label_pos_recent, use_markup: true, halign: Gtk.Align.START }));
+		let labelPosRecent = _("Display of recent emojis :");
+		this.add(new Gtk.Label({ label: labelPosRecent, use_markup: true, halign: Gtk.Align.START }));
 
-		let align = new Gtk.Alignment({ left_padding: 30 });
+		let align = new Gtk.Alignment({ left_padding: 40 });
 		this.add(align);
 
-		let grid = new Gtk.Grid({ orientation: Gtk.Orientation.VERTICAL, row_spacing: 12, column_spacing: 12 });
-		align.add(grid);
+		let radioBox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, expand: true, spacing: 12 });
+		align.add(radioBox);
 		
-		let radio_top = null;
-		radio_top = new Gtk.RadioButton({ group: radio_top, label: _("Top"), valign: Gtk.Align.START });
-		let radio_bottom = new Gtk.RadioButton({ group: radio_top, label: _("Bottom"), valign: Gtk.Align.START });
-		let radio_none = new Gtk.RadioButton({ group: radio_top, label: _("Do not display"), valign: Gtk.Align.START });
+		//radioTop is both the first Gtk.RadioButton and the group in which the 3 buttons belong
+		let radioTop = null;
+		radioTop = new Gtk.RadioButton({ group: radioTop, label: _("Top"), valign: Gtk.Align.START });
+		let radioBottom = new Gtk.RadioButton({ group: radioTop, label: _("Bottom"), valign: Gtk.Align.START });
+		let radioNone = new Gtk.RadioButton({ group: radioTop, label: _("Do not display"), valign: Gtk.Align.START });
 		
+		//getting the current 'position' setting state, before an eventual modification
 		let position_s = this._settings.get_string('position');
-		
+		//displaying this current state to the user
 		if(position_s === 'top') {
-			radio_top.set_active(true);
+			radioTop.set_active(true);
 		} else if (position_s === 'bottom') {
-			radio_bottom.set_active(true);
+			radioBottom.set_active(true);
 		} else {
-			radio_none.set_active(true);
+			radioNone.set_active(true);
 		}
 		
-		radio_top.connect('toggled', Lang.bind(this, function(widget) {
+		radioTop.connect('toggled', Lang.bind(this, function(widget) {
 			if (widget.active) {
 				this._settings.set_string('position', 'top');
 			}
 		}));
 		
-		radio_bottom.connect('toggled', Lang.bind(this, function(widget) {
+		radioBottom.connect('toggled', Lang.bind(this, function(widget) {
 			if (widget.active) {
 				this._settings.set_string('position', 'bottom');
 			}
 		}));
 		
-		radio_none.connect('toggled', Lang.bind(this, function(widget) {
+		radioNone.connect('toggled', Lang.bind(this, function(widget) {
 			if (widget.active) {
 				this._settings.set_string('position', 'none');
 			}
 		}));
 		
-		grid.add(radio_top);
-		grid.add(radio_bottom);
-		grid.add(radio_none);
+		radioBox.add(radioTop);
+		radioBox.add(radioBottom);
+		radioBox.add(radioNone);
 		
 		//-------------------------------------------------------
 		
-		let label_nb_recent = _("Number of recently used emojis :");
-		this.add(new Gtk.Label({ label: label_nb_recent, use_markup: true, halign: Gtk.Align.START }));
+		let labelNbRecents = _("Number of recently used emojis :");
 		
 		let nbRecents = new Gtk.SpinButton();
         nbRecents.set_sensitive(true);
@@ -91,16 +97,20 @@ const EmojiSettingsWidget = new GObject.Class({
 		nbRecents.set_value(12);
         nbRecents.set_value(this._settings.get_int('nbrecents'));
         nbRecents.set_increments(1, 2);
+        
 		nbRecents.connect('value-changed', Lang.bind(this, function(w){
-			let value2 = w.get_value_as_int();
-			this._settings.set_int('nbrecents', value2);
+			var value = w.get_value_as_int();
+			this._settings.set_int('nbrecents', value);
 		}));
-		this.add(nbRecents);
+		
+		let recentBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 15 });
+		recentBox.pack_start(new Gtk.Label({ label: labelNbRecents, use_markup: true, halign: Gtk.Align.START }), false, false, 0);
+		recentBox.pack_end(nbRecents, false, false, 0);
+		this.add(recentBox);
 		
 		//-------------------------------------------------------
 		
-		let label_nb_recent = _("Number of emojis on one line :");
-		this.add(new Gtk.Label({ label: label_nb_recent, use_markup: true, halign: Gtk.Align.START }));
+		let labelNbEmojis = _("Number of emojis on one line :");
 		
 		let nbCols = new Gtk.SpinButton();
         nbCols.set_sensitive(true);
@@ -108,16 +118,23 @@ const EmojiSettingsWidget = new GObject.Class({
 		nbCols.set_value(15);
 		nbCols.set_value(this._settings.get_int('nbcols'));
         nbCols.set_increments(1, 2);
+        
 		nbCols.connect('value-changed', Lang.bind(this, function(w){
-			let value = w.get_value_as_int();
+			var value = w.get_value_as_int();
 			this._settings.set_int('nbcols', value);
 		}));
-		this.add(nbCols);
 		
-		//-----------------------------------
+		let colsBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 15 });
+		colsBox.pack_start(new Gtk.Label({ label: labelNbEmojis, use_markup: true, halign: Gtk.Align.START }), false, false, 0);
+		colsBox.pack_end(nbCols, false, false, 0);
+		this.add(colsBox);
 	}
 });
 
+//-----------------------------------------------
+
+//I guess this is like the "enable" in extension.js : something called each
+//time he user try to access the settings' window
 function buildPrefsWidget() {
     let widget = new EmojiSettingsWidget();
     widget.show_all();
